050cd4cc8e01d61212c30b30dd005d9993912ad4
Kadir Kesimal <kadir.kesimal@uzay.tubitak.gov.tr>
2023-05-22 22:40:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialCardView geographycard, historycard,artcard,literaturecard,moviecard,sciencecard;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        geographycard = findViewById(R.id.Geographycard);
+        historycard = findViewById(R.id.HistoryCard);
+        artcard = findViewById(R.id.ArtCard);
+        literaturecard = findViewById(R.id.LiteratureCard);
+        moviecard = findViewById(R.id.MovieCard);
+        sciencecard = findViewById(R.id.ScienceCard);
+
+
+        geographycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GeographyQuiz.class));
+                finish();
+            }
+
+        });
+        historycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HistoryQuiz.class));
+                finish();
+            }
+
+        });
+        artcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ArtQuiz.class));
+                finish();
+            }
+
+        });
+        literaturecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LiteratureQuiz.class));
+                finish();
+            }
+
+        });
+        moviecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MovieQuiz.class));
+                finish();
+            }
+
+        });
+        sciencecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ScienceQuiz.class));
+                finish();
+            }
+
+        });
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(MainActivity.this);
+        materialAlertDialogBuilder.setTitle(R.string.app_name);
+        materialAlertDialogBuilder.setMessage("Çıkmak istediğinizden emin misiniz ?");
+
+        materialAlertDialogBuilder.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        materialAlertDialogBuilder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog = materialAlertDialogBuilder.create();
+        alertDialog.show();
+}
+}
